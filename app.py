94463cde31c9bc0deb5f8f3a1de063e5023e8e1d
Marcos <marcos.fuentes@uc.cl>
2025-08-15 13:28:17 -0400

    cambios

--- app.py
+++ app.py
@@ -533,4 +533,5 @@ async def handle_media_stream(websocket: WebSocket):
 # Main
 # ------------------------
 if __name__ == "__main__":
+    
     uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
